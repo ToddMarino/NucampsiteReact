e7d15a56596ff26cc4821dac85abb2f8af3067c8
Todd Marino <todd.j.marino@gmail.com>
2021-12-13 18:27:10 -0600

    Set Up Part 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
